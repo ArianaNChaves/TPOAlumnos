72c6784c3c9e6445e6e52297a09a74ada2aced26
ArianaNChaves <navi.ariana99@gmail.com>
2023-05-01 16:39:41 -0300

    migracion a netbeans

---
+++ TPAlumnos/src/main/java/com/mycompany/tpalumnos/TPAlumnos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpalumnos;
+
+/**
+ *
+ * @author Ariana
+ */
+public class TPAlumnos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
