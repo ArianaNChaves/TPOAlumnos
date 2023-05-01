f333273699d143bc07fe24221a04114f96cf1ff7
ArianaNChaves <navi.ariana99@gmail.com>
2023-05-01 18:18:50 -0300

    proyecto finalizado sin vistas

---
+++ TPAlumnos/src/main/java/com/mycompany/tpalumnos/Alumno.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tpalumnos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alumno {
+    Integer legajo;
+    String apellido;
+    String nombre;
+    List<Materia> materias;
+
+    public Alumno(Integer legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        materias = new ArrayList<>();
+    }
+
+    public Integer getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(Integer legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void agregarMateria(Materia m) {
+        if (!materias.contains(m)) {
+            this.materias.add(m);
+        } else {
+            System.out.printf("El alumno %s ya esta inscripto en la materia %s", this.nombre, m.getNombre());
+        }
+
+    }
+
+    public Integer cantidadMaterias() {
+        return materias.size();
+    }
+}
--- TPAlumnos/src/main/java/com/mycompany/tpalumnos/Main.java
+++ TPAlumnos/src/main/java/com/mycompany/tpalumnos/Main.java
@@ -1,5 +1,8 @@
+package com.mycompany.tpalumnos;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("xd");
+        Alumno alumno = new Alumno(123, "juan", "juan");
+        Materia materia = new Materia(1, "matematica", 1);
+        alumno.agregarMateria(materia);
     }
 }
