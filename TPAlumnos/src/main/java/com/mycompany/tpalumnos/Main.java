f78eb262db6c7c797e1568a88e5cc45a728ffc7d
Mauro Corvalan <maurocorvaa@gmail.com>
2023-05-06 00:35:45 -0300

    Update Main.java

--- TPAlumnos/src/main/java/com/mycompany/tpalumnos/Main.java
+++ TPAlumnos/src/main/java/com/mycompany/tpalumnos/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Alumno alumno = new Alumno(123, "juan", "juan");
         Materia materia = new Materia(1, "matematica", 1);
-        
     }
 }
